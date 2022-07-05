6f99cd0a18c37506d7b358eb4b5e2fc7d40be460
israelmark1 <israelmark98@gmail.com>
2022-07-05 13:46:47 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const sendMassage = () => {
+
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder='massage...'></input>
+      <button onClick={sendMassage}>send massage</button>
     </div>
   );
 }
