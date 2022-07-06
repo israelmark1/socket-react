97071654e2101f1dd90ef5b1289b025e64fc56a5
israelmark1 <israelmark98@gmail.com>
2022-07-06 13:56:13 +0300

    done

--- src/App.js
+++ src/App.js
@@ -5,19 +5,36 @@ import { useEffect, useState } from "react";
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const joinRoom = () => {
+    if (room !== "") {
+      alert(`you joined to room ${room}`);
+      socket.emit("join_room", room);
+    }
+  };
 
   const sendMessage = () => {
-    socket.emit("send_message", { message });
+    socket.emit("send_message", { message, room });
   };
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      alert(data.message);
+      setMessageReceived(data.message);
     });
   }, [socket]);
   return (
     <div className="App">
+      <input
+        placeholder="room number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>Join Room</button>
       <input
         placeholder="message..."
         onChange={(event) => {
@@ -25,6 +42,8 @@ const [message, setMessage] = useState("");
         }}
       ></input>
       <button onClick={sendMessage}>send message</button>
+      <h1>Message:</h1>
+      {messageReceived}
     </div>
   );
 }
